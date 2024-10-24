68ac2a31fbe2adbf5eb47f1b7a82409cc2c09126
landonAL <intellectisbest@gmail.com>
2024-10-24 17:56:44 -0500

    Began adding wallpapers

---
+++ wallpapers/randomize.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Kill any previously running process to prevent multiple daemons causing lag
+killall swaybg
+
+# Directory containing wallpapers
+WALLPAPER_DIR="$HOME/Wallpapers"
+
+# Select a random wallpaper from all subdirectories
+RANDOM_WALLPAPER=$(find "$WALLPAPER_DIR" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" \) | shuf -n 1)
+
+# Set the wallpaper using swaybg in a non-blocking way
+swaybg -i "$RANDOM_WALLPAPER" -m fill &
